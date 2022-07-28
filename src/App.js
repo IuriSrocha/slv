6403f69b3c02efc380694d96a82a9108a05b7d32
IuriSrocha <iurisrocha.ir@gmail.com>
2022-07-28 20:33:35 -0300

    1 passo
    dsdsd

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { FormBook } from './components/FormBook';
+import { NavBar } from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <FormBook />
+      
+    </div>
+  );
+}
+
+export default App;
