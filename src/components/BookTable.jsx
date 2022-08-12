18a59bba436fe94722d9d54ce4135cb4faecf98b
IuriSrocha <iurisrocha.ir@gmail.com>
2022-08-11 21:12:11 -0300

    gdsd
    fdfaefdas

--- src/components/BookTable.jsx
+++ src/components/BookTable.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import BootstrapTable from 'react-bootstrap-table-next'
 
 export default function BookTable() {
-  return (
-    <div>
-        <BootstrapTable keyField='id' data={ products } columns={ columns } />
-    </div>
-    
-  )
+  let [posts, setPosts] = useState([])
+
+  const columns = [
+    {
+      datafild: 'id'
+      text: 'Id'
+    }, {
+      dataField: 'title',
+      text: 'title'
+    }, {
+      dataField: 'body',
+      text: 'texto'
+    }];
+
+
+  const getPosts = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response = response.jason())
+      .then(data => {
+        useEffect(data)
+      })
+  }
+
+  useEffect(() => {
+
+  }, []);
+
+
+}]
+
+return {(
+  <div>
+    <BootstrapTable keyField='id' data={posts} columns={[]} />
+  </div>
+
+)
 }
